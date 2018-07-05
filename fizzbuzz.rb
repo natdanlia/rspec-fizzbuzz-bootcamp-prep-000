74bab036e300e1c764fc94fc22c0a73a14de41f1
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-07-05 22:11:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,12 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   #binding.pry
-  a = int % 3
-  b = int % 5
-  if a == 0
+  if int % 3 && int % 5 == 0
+    "fizzbuzz"
+  elsif int % 3 == 0
     "Fizz"
-  elsif b == 0
+  elsif int % 5 == 0
      "Buzz"
-  elsif  a == 0 b == 0
-     "FizzBuzz"
   else 
     puts nil
   end
