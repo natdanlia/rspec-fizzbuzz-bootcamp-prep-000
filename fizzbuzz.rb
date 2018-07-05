e440218c59dddf718d96dcc0e698173cc544377e
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-07-05 21:47:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,15 @@
+require 'pry'
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+def fizzbuzz(int)
+  binding.pry
+  if int % 3 == 0
+    "Fizz"
+  else if int % 5 == 0
+     "Buzz"
+  else if int % 5 == 0 && int % 3 == 0 
+     "FizzBuzz"
+  else 
+    puts nil
+  end
+end
